642ebf821bc6e47f4abd6ff9e3a55dba9ce6cc3f
gspider8 <83229711+gspider8@users.noreply.github.com>
2024-08-01 11:52:52 -0400

    working s3 static website

---
+++ frontend/index.js
@@ -0,0 +1,163 @@
+const projectCards = document.getElementById("project-cards");
+const typeFilter = document.getElementById("type-filter");
+const languageFilter = document.getElementById("language-filter");
+const frameworkFilter = document.getElementById("framework-filter");
+let myData
+
+const fetchJSON = () => {
+  const pathJSON = "./fcc-javascript/fcc-javascript.json"
+  fetch(pathJSON).then(
+      response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      }
+  ).then(data => {
+    myData = data;
+    console.log("My Data:", myData);
+    renderProjectCards(myData["portfolio"]);
+  }).catch(() => {
+    this.dataError = true;
+  });
+}
+fetchJSON()
+
+
+const capitalizeEachWord = (str) => str.split("-").map(
+  word => word[0].toUpperCase() + word.substring(1)
+).join("-")
+
+function renderProjectCards(objArray) {
+  objArray.forEach((obj) => {
+    projectCards.innerHTML += `
+      <a href="${obj.id}/"><div class="project-card">
+        <h2>${obj.name}</h2>
+        <p>${capitalizeEachWord(obj.type)} Project</p>
+        <p>${obj.description}</p>
+        <p>Languages: ${obj.languages}</p>
+        <p>
+          <a href="https://github.com/gspider8/fcc-javascript/tree/main/${obj.number}-${obj.id}/"
+            target="_blank">Source Code
+          </a> | 
+          <a href="fcc-javascript/${obj.number}-${obj.id}/index.html">Preview</a></p>
+      </div></a>`
+  })
+}
+
+const filterProjects = () => {
+  projectCards.innerHTML = "";
+
+  const filterByType = (projectCard) => {
+    if (typeFilter.value === "all" || typeFilter.value === "0") {
+      return true
+    } else {
+      return typeFilter.value === projectCard.type;
+    }
+  }
+
+  const filterByLanguage = (projectCard) => {
+    if (languageFilter.value === "all" || languageFilter.value === "0") {
+      return true;
+    } else {
+      console.log(projectCard.languages.includes(languageFilter.value));
+      console.log(languageFilter.value)
+      return projectCard.languages.includes(languageFilter.value);
+      // const languagesArray = projectCard.languages.toLowerCase().split(", ")
+      // return languagesArray.includes(languageFilter.value);
+    }
+  }
+
+  const filterByFramework = (projectCard) => {
+    if (frameworkFilter.value === "all" || frameworkFilter.value === "0") {
+      return true
+    } else if (projectCard.hasOwnProperty("frameworks")) {
+      console.log()
+      return projectCard?.frameworks.includes(frameworkFilter.value);
+    } else {
+      return false
+    }
+  }
+
+  renderProjectCards(myData["portfolio"].filter(
+    project => filterByType(project)
+      && filterByLanguage(project)
+      && filterByFramework(project)
+  ))
+}
+
+
+typeFilter.addEventListener("change", filterProjects)
+languageFilter.addEventListener("change", filterProjects)
+frameworkFilter.addEventListener("change", filterProjects)
+
+// Archive
+// console.log(myPortfolio);
+// myPortfolio = [
+//   {
+//     name: "Dragon Fighting RPG",
+//     id: "dragon-rpg",
+//     type: "front-end",
+//     description: "FreeCodeCamp JavaScript Course",
+//     languages: "HTML, CSS, JavaScript",
+//     number: 3,
+//     frameworks: []
+//   },
+//   {
+//     name: "Random Background Color",
+//     id: "random-background-color",
+//     type: "front-end",
+//     description: "FreeCodeCamp JavaScript Course",
+//     languages: "HTML, CSS, JavaScript",
+//     number: 4
+//   },
+//   {
+//     name: "Calorie Counter",
+//     id: "calorie-counter",
+//     type: "front-end",
+//     description: "FreeCodeCamp JavaScript Course",
+//     languages: "HTML, CSS, JavaScript",
+//     number: 5
+//   },
+//   {
+//     name: "Rock Paper Scissors",
+//     id: "rock-paper-scissors",
+//     type: "front-end",
+//     description: "FreeCodeCamp JavaScript Course",
+//     languages: "HTML, CSS, JavaScript",
+//     number: 6
+//   },
+//   {
+//     name: "Music Player",
+//     id: "music-player",
+//     type: "front-end",
+//     description: "FreeCodeCamp JavaScript Course",
+//     languages: "HTML, CSS, JavaScript",
+//     number: 7
+//   },
+//   {
+//     name: "Football Cards",
+//     id: "football-cards",
+//     type: "front-end",
+//     description: "FreeCodeCamp JavaScript Course",
+//     languages: "HTML, CSS, JavaScript",
+//     number: 10
+//   },
+//   {
+//     name: "Statistics Calculator",
+//     id: "statistics-calculator",
+//     type: "front-end",
+//     description: "FreeCodeCamp JavaScript Course",
+//     languages: "HTML, CSS, JavaScript",
+//     number: 16
+//   },
+//   {
+//     name: "Bookstore",
+//     id: "bookstore",
+//     type: "back-end",
+//     description: "Django for Professionals Course",
+//     languages: "Python, HTML",
+//     frameworks: ["django", "tailwind"]
+//   }
+// ]
+// renderProjectCards(myPortfolio);
